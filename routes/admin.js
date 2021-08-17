21bd40b7b277a1df1460c6cf6e82c4b264cc1d5a
dakshya-mahesh <78347117+dakshya-mahesh@users.noreply.github.com>
2021-08-17 19:58:23 +0545

    style the component
    style the component

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,20 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
 
-const rootDir = require('../util/path')
+const express = require('express');
 
-const router = express.Router()
+const rootDir = require('../util/path');
 
-// @route: /admin/add-product
-// @method: GET
- 
-router.get('/add-product',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-})
+const router = express.Router();
 
-// @route: /admin/add-product
-// @method: Post
-router.post('/add-product',(req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
 
-module.exports = router
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,13 @@
-const path = require('path')
-const express =require('express')
+const path = require('path');
 
-const rootDir = require('../util/path')
+const express = require('express');
 
-const router = express.Router()
+const rootDir = require('../util/path');
 
-console.log(rootDir)
+const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-})
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
 
-module.exports = router
+module.exports = router;
