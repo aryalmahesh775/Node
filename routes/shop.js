1b9946670d6a5e63cf5dfe838ddcf59d4ec2438d
dakshya-mahesh <78347117+dakshya-mahesh@users.noreply.github.com>
2021-08-17 07:28:11 +0545

    using Helper function for navigation
    using Helper function for navigation

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,14 @@
 const path = require('path')
 const express =require('express')
 
+const rootDir = require('../util/path')
+
 const router = express.Router()
-console.log(__dirname)
+
+console.log(rootDir)
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 })
 
 module.exports = router
