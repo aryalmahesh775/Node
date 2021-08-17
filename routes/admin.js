bb0b2a26258869dfad7f69dabf8aa2811cfa643e
dakshya-mahesh <78347117+dakshya-mahesh@users.noreply.github.com>
2021-08-17 07:03:32 +0545

    serving the html page
    static page

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 
 const router = express.Router()
@@ -6,8 +7,7 @@ const router = express.Router()
 // @method: GET
  
 router.get('/add-product',(req, res, next) => {
-    console.log('This is add product')
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+    res.sendFile(path.join(__dirname,'../', 'views', 'add-product.html'));
 })
 
 // @route: /admin/add-product
